e7eb7b550fd7a1ac5e8f26a79d8cc999a6eebec1
shohibunnajam <shohibunnajam@gmail.com>
2025-12-04 20:28:56 +0700

    page footer

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -6,30 +6,32 @@ import Image from "next/image";
 export default function Footer() {
   return (
     <div className="flex justify-center items-center bg-white mt-20">
-      <div className="w-10/12">
+      <div className="w-10/12 p-5">
         <div className="grid grid-cols-4 gap-4">
-          <div className="col-span-1">
+          <div className="col-span-1 text-sm">
             <p>
               Jalan Pakusari No. 23 Kabupaten Jember
               <br />
               Pakusari 68181
             </p>
 
-            <p>pyoga3199@gmail.com</p>
+            <p className="mt-1">pyoga3199@gmail.com</p>
 
-            <p>+62 815-4911-8907</p>
+            <p className="mt-2">+62 815-4911-8907</p>
           </div>
 
           <div className="col-span-1">
-            <p>Layanan Kula</p>
+            <h5 className="text-sm">Layanan Kula</h5>
 
-            <p>Karya</p>
+            <p className="text-xs">Karya</p>
           </div>
 
           <div className="col-span-1">
-            <div className="flex justify-between items-center">
+            <h5 className="text-sm">Nuwun pirsa</h5>
+
+            <div className="flex justify-start items-center">
               <Link href={"https://www.youtube.com/@Bhaya-ngan"}>
-                <Youtube />
+                <Youtube className="w-7 h-7 mr-1" />
               </Link>
 
               <a href="mailto:pyoga3199@gmail.com">
@@ -40,11 +42,11 @@ export default function Footer() {
 
           <div className="col-span-1">
             <div className="flex justify-start items-center">
-              <Copyright />
-              <p>Copyright Bhaya 2025</p>
+              <Copyright className="w-5 h-5 mr-1" />
+              <p className="text-sm">Copyright Bhaya 2025</p>
             </div>
 
-            <Image src={Logo} alt="Logo" />
+            <Image src={Logo} alt="Logo" className="w-20 h-20 mt-2" />
           </div>
         </div>
       </div>
